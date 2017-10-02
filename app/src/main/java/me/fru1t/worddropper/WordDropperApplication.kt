1e929845c75c7c3bf2c5750eb07953f30a87120a
kodlee yin <kodlee@fru1t.me>
2017-10-01 21:51:42 -0700

    Converted WordDropperApplication to kotlin (#24)

---
+++ app/src/main/java/me/fru1t/worddropper/WordDropperApplication.kt
@@ -0,0 +1,87 @@
+package me.fru1t.worddropper
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.support.annotation.StringRes
+
+import java.util.HashSet
+
+import me.fru1t.worddropper.database.DatabaseUtils
+import me.fru1t.worddropper.settings.ColorTheme
+import me.fru1t.worddropper.settings.Dictionary
+import me.fru1t.worddropper.settings.colortheme.ColorThemeEventHandler
+
+/**
+ * Settings and global variables (*gasp*) loaded onWrapEventListener startup.
+ */
+class WordDropperApplication : Application(), SharedPreferences.OnSharedPreferenceChangeListener {
+    companion object {
+        const val LOG_TAG = "WordDropper"
+    }
+
+    private val colorThemeEventHandlers: HashSet<ColorThemeEventHandler> = HashSet()
+
+    private val sharedPreferences by lazy {
+        getSharedPreferences(
+                resources.getString(R.string.app_sharedPreferencesFileName), Context.MODE_PRIVATE)
+    }
+    val dictionary: Dictionary by lazy { Dictionary(this) }
+    val databaseUtils: DatabaseUtils by lazy { DatabaseUtils(applicationContext) }
+    val isDebugging: Boolean
+        get() = resources.getBoolean(R.bool.app_debug)
+
+    private var colorTheme: ColorTheme? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        updateFromSettings()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        updateFromSettings()
+    }
+
+    /**
+     * Retrieves a string preference given a key and default value.
+     * @param prefKey The preference name given via string resource id.
+     * @param defaultValue The default value the setting should be if it doesn't exist.
+     * @return The string preference.
+     */
+    fun getStringPreference(@StringRes prefKey: Int, defaultValue: String): String? =
+            sharedPreferences.getString(resources.getString(prefKey), defaultValue)
+
+    /**
+     * Applies a preference given its preference key and value.
+     * @param prefKey The preference name given via string resource id.
+     * @param value The value to store.
+     */
+    fun putStringPreference(@StringRes prefKey: Int, value: String) {
+        sharedPreferences.edit().putString(resources.getString(prefKey), value).apply()
+    }
+
+    /**
+     * Adds a color theme event handler, triggering the
+     * [ColorThemeEventHandler.onColorThemeChange] upon successful registration.
+     */
+    fun addColorThemeEventHandler(handler: ColorThemeEventHandler) {
+        if (colorThemeEventHandlers.add(handler)) {
+            handler.onColorThemeChange(colorTheme!!)
+        }
+    }
+
+    fun removeColorThemeEventHandler(handler: ColorThemeEventHandler) {
+        colorThemeEventHandlers.remove(handler)
+    }
+
+    private fun updateFromSettings() {
+        val newTheme = ColorTheme.valueOf(
+                getStringPreference(R.string.pref_colorTheme, ColorTheme.INVERSE_ORANGE.name)!!)
+        if (newTheme != colorTheme) {
+            colorTheme = newTheme
+            colorThemeEventHandlers.forEach { handler -> handler.onColorThemeChange(newTheme) }
+        }
+    }
+}
--- app/src/main/java/me/fru1t/worddropper/database/DatabaseUtils.java
+++ app/src/main/java/me/fru1t/worddropper/database/DatabaseUtils.java
@@ -1,102 +1,99 @@
-package me.fru1t.worddropper.database;
+package me.fru1t.worddropper.database
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.BaseColumns;
-import android.support.annotation.Nullable;
-import android.util.Log;
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+import android.util.Log
 
-import java.util.ArrayList;
-import java.util.function.Consumer;
+import java.util.ArrayList
+import java.util.function.Consumer
 
-import me.fru1t.android.database.Row;
-import me.fru1t.worddropper.WordDropperApplication;
-import me.fru1t.worddropper.database.tables.Game;
-import me.fru1t.worddropper.database.tables.GameWord;
-import me.fru1t.worddropper.settings.Difficulty;
+import me.fru1t.android.database.Row
+import me.fru1t.worddropper.WordDropperApplication
+import me.fru1t.worddropper.database.tables.Game
+import me.fru1t.worddropper.database.tables.GameWord
+import me.fru1t.worddropper.settings.Difficulty
 
-import static me.fru1t.worddropper.WordDropperApplication.LOG_TAG;
+import me.fru1t.worddropper.WordDropperApplication.LOG_TAG
 
 /**
  * Methods to manipulate and retrieve data from the database.
  */
-public class DatabaseUtils extends SQLiteOpenHelper {
+class DatabaseUtils(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
-    private static final String DATABASE_NAME = "word_dropper.db";
-    private static final int DATABASE_VERSION = 1;
+    private val app: WordDropperApplication
 
-    private static final String DROP_TABLE = "DROP TABLE IF EXISTS ";
-
-    private final WordDropperApplication app;
-
-    public DatabaseUtils(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        app = (WordDropperApplication) context.getApplicationContext();
+    init {
+        app = context.applicationContext as WordDropperApplication
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(Game.CREATE_TABLE);
-        db.execSQL(GameWord.CREATE_TABLE);
-        Log.i(LOG_TAG, "Initial database creation successful");
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(Game.CREATE_TABLE)
+        db.execSQL(GameWord.CREATE_TABLE)
+        Log.i(LOG_TAG, "Initial database creation successful")
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
-    @Override
-    public void onOpen(SQLiteDatabase db) {
-        if (app.isDebugging()) {
-            db.execSQL(DROP_TABLE + Game.TABLE_NAME);
-            db.execSQL(DROP_TABLE + GameWord.TABLE_NAME);
-            Log.d(LOG_TAG, "Cleared database for debug");
-
-            db.execSQL(Game.CREATE_TABLE);
-            db.execSQL(GameWord.CREATE_TABLE);
+    override fun onOpen(db: SQLiteDatabase) {
+        if (app.isDebugging) {
+            resetDatabase()
+            Log.d(LOG_TAG, "Cleared database for debug")
         }
+    }
+
+    /**
+     * Deletes all data off of the tables by dropping them and re-creating.
+     */
+    fun resetDatabase() {
+        val db = writableDatabase
+        db.execSQL(DROP_TABLE + Game.TABLE_NAME)
+        db.execSQL(DROP_TABLE + GameWord.TABLE_NAME)
+        db.execSQL(Game.CREATE_TABLE)
+        db.execSQL(GameWord.CREATE_TABLE)
     }
 
     /**
      * Builds the necessary rows to start a new game within the database.
      * @return The game ID.
      */
-    public long startGame(Difficulty difficulty, String boardState, int movesEarned,
-                          int scramblesEarned) {
-        ContentValues values = new ContentValues();
+    fun startGame(difficulty: Difficulty, boardState: String, movesEarned: Int,
+                  scramblesEarned: Int): Long {
+        val values = ContentValues()
 
         // We store phone time here, be sure to sanitize onWrapEventListener server
-        values.put(Game.COLUMN_UNIX_START, System.currentTimeMillis() / 1000);
-        values.put(Game.COLUMN_STATUS, Game.STATUS_IN_PROGRESS);
-        values.put(Game.COLUMN_DIFFICULTY, difficulty.toString());
-        values.put(Game.COLUMN_BOARD_STATE, boardState);
-        values.put(Game.COLUMN_MOVES_EARNED, movesEarned);
-        values.put(Game.COLUMN_SCRAMBLES_EARNED, scramblesEarned);
-        values.put(Game.COLUMN_SCRAMBLES_USED, 0);
-        values.put(Game.COLUMN_LEVEL, 1);
+        values.put(Game.COLUMN_UNIX_START, System.currentTimeMillis() / 1000)
+        values.put(Game.COLUMN_STATUS, Game.STATUS_IN_PROGRESS)
+        values.put(Game.COLUMN_DIFFICULTY, difficulty.toString())
+        values.put(Game.COLUMN_BOARD_STATE, boardState)
+        values.put(Game.COLUMN_MOVES_EARNED, movesEarned)
+        values.put(Game.COLUMN_SCRAMBLES_EARNED, scramblesEarned)
+        values.put(Game.COLUMN_SCRAMBLES_USED, 0)
+        values.put(Game.COLUMN_LEVEL, 1)
 
-        return getWritableDatabase().insert(Game.TABLE_NAME, null, values);
+        return writableDatabase.insert(Game.TABLE_NAME, null, values)
     }
 
     /**
      * End a game.
      * @param gameId The game to end.
      */
-    public void endGame(long gameId) {
-        ContentValues values = new ContentValues();
-        values.put(Game.COLUMN_STATUS, Game.STATUS_COMPLETED);
-        Game.updateById(getWritableDatabase(), values, gameId);
+    fun endGame(gameId: Long) {
+        val values = ContentValues()
+        values.put(Game.COLUMN_STATUS, Game.STATUS_COMPLETED)
+        Game.updateById(writableDatabase, values, gameId)
     }
 
     /**
      * Performs an update on the given game.
      */
-    public void updateGame(long gameId, Consumer<ContentValues> update) {
-        ContentValues gameValues = new ContentValues();
-        update.accept(gameValues);
-        Game.updateById(getWritableDatabase(), gameValues, gameId);
+    fun updateGame(gameId: Long, update: Consumer<ContentValues>) {
+        val gameValues = ContentValues()
+        update.accept(gameValues)
+        Game.updateById(writableDatabase, gameValues, gameId)
     }
 
     /**
@@ -108,23 +105,23 @@ public class DatabaseUtils extends SQLiteOpenHelper {
      * @param newBoardState The new board state of the game.
      * @return Whether or not the database was updated.
      */
-    public boolean addGameMove(long gameId, String word, int pointValue, int score,
-                               String newBoardState) {
+    fun addGameMove(gameId: Long, word: String, pointValue: Int, score: Int,
+                    newBoardState: String): Boolean {
         // Insert into GameWord
-        ContentValues gameWordValues = new ContentValues();
-        gameWordValues.put(GameWord.COLUMN_GAME_ID, gameId);
-        gameWordValues.put(GameWord.COLUMN_POINT_VALUE, pointValue);
-        gameWordValues.put(GameWord.COLUMN_WORD, word);
-        if (getWritableDatabase().insert(GameWord.TABLE_NAME, null, gameWordValues) == -1) {
-            return false;
+        val gameWordValues = ContentValues()
+        gameWordValues.put(GameWord.COLUMN_GAME_ID, gameId)
+        gameWordValues.put(GameWord.COLUMN_POINT_VALUE, pointValue)
+        gameWordValues.put(GameWord.COLUMN_WORD, word)
+        if (writableDatabase.insert(GameWord.TABLE_NAME, null, gameWordValues) == -1) {
+            return false
         }
 
         // Update board state in Game
-        ContentValues gameValues = new ContentValues();
-        gameValues.put(Game.COLUMN_BOARD_STATE, newBoardState);
-        Game.updateById(getWritableDatabase(), gameValues, gameId);
+        val gameValues = ContentValues()
+        gameValues.put(Game.COLUMN_BOARD_STATE, newBoardState)
+        Game.updateById(writableDatabase, gameValues, gameId)
 
-        return true;
+        return true
     }
 
     /**
@@ -132,25 +129,23 @@ public class DatabaseUtils extends SQLiteOpenHelper {
      * @param gameId The game to fetch.
      * @return A list of moves, in order, performed in the game.
      */
-    public ArrayList<String> getGameMoves(long gameId) {
-        ArrayList<String> result = new ArrayList<>();
+    fun getGameMoves(gameId: Long): ArrayList<String> {
+        val result = ArrayList<String>()
 
-        Cursor c = getReadableDatabase().query(
+        val c = readableDatabase.query(
                 GameWord.TABLE_NAME,
-                new String[] { GameWord.COLUMN_WORD },
+                arrayOf(GameWord.COLUMN_WORD),
                 GameWord.COLUMN_GAME_ID + " = ?",
-                new String[] { gameId + "" },
-                null,
-                null,
-                GameWord._ID + " ASC");
+                arrayOf(gameId.toString() + ""), null, null,
+                GameWord._ID + " ASC")
 
         if (c.moveToFirst()) {
             do {
-                result.add(c.getString(0));
-            } while (c.moveToNext());
+                result.add(c.getString(0))
+            } while (c.moveToNext())
         }
-        c.close();
-        return result;
+        c.close()
+        return result
     }
 
     /**
@@ -160,27 +155,23 @@ public class DatabaseUtils extends SQLiteOpenHelper {
      * @param columns The columns to fetch.
      * @return A map keyed by the column name or null if the row wasn't found.
      */
-    @Nullable
-    public Row getRowFromId(String tableName, long id, String[] columns) {
-        Cursor c = getReadableDatabase().query(
+    fun getRowFromId(tableName: String, id: Long, columns: Array<String>): Row? {
+        val c = readableDatabase.query(
                 tableName,
                 columns,
                 BaseColumns._ID + " = ?",
-                new String[] { id + "" },
-                null,
-                null,
-                null);
+                arrayOf(id.toString() + ""), null, null, null)
         if (!c.moveToFirst()) {
-            return null;
+            return null
         }
 
-        Row result = new Row();
-        for (String column : columns) {
-            result.put(column, c.getString(c.getColumnIndex(column)));
+        val result = Row()
+        for (column in columns) {
+            result.put(column, c.getString(c.getColumnIndex(column)))
         }
 
-        c.close();
-        return result;
+        c.close()
+        return result
     }
 
     /**
@@ -191,21 +182,29 @@ public class DatabaseUtils extends SQLiteOpenHelper {
      * @param action What to do with the row.
      * @return False if the query returned no results; otherwise, true.
      */
-    public boolean forEachResult(String query, @Nullable String[] args, Consumer<Cursor> action) {
-        Cursor c = null;
+    fun forEachResult(query: String, args: Array<String>?, action: Consumer<Cursor>): Boolean {
+        var c: Cursor? = null
         try {
-            c = getReadableDatabase().rawQuery(query, args);
-            if (!c.moveToFirst()) {
-                return false;
+            c = readableDatabase.rawQuery(query, args)
+            if (!c!!.moveToFirst()) {
+                return false
             }
             do {
-                action.accept(c);
-            } while (c.moveToNext());
-            return true;
+                action.accept(c)
+            } while (c.moveToNext())
+            return true
         } finally {
             if (c != null) {
-                c.close();
+                c.close()
             }
         }
+    }
+
+    companion object {
+
+        private val DATABASE_NAME = "word_dropper.db"
+        private val DATABASE_VERSION = 1
+
+        private val DROP_TABLE = "DROP TABLE IF EXISTS "
     }
 }
